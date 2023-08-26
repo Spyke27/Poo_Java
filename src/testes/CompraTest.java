dfdf6c98e35e2b14793c0c241f5ca74266fa0f8d
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-26 13:09:53 -0300

    test: testing new order

--- src/testes/CompraTest.java
+++ src/testes/CompraTest.java
@@ -18,12 +18,17 @@ public class CompraTest {
 		List<Product> list = new ArrayList<>();
 		Client client = new Client(3, "Chris Molthes", "chris@gmail.com");
 		// Creating products
-		Product product = new Product("Acer Nitro", 5800.50, 3);
+		Product product = new Product("Acer Nitro", 10.00, 2);
 		// Creating compra and adding product
 		Compra compra = new Compra(1, client, list);
 		compra.addProduct(product);
-	     
-	     Assertions.assertEquals(2, product.getQuantity());
-	     Assertions.assertEquals(5800.50, compra.totalOrder());
+		compra.addProduct(product);
+		
+		Assertions.assertEquals(0, product.getQuantity());
+		Assertions.assertEquals(20.00, compra.totalOrder());
+		
+	     // Trying add product without stock
+		 compra.addProduct(product);
+	     Assertions.assertEquals("Acer Nitro Unavailable!", product.getName() + " Unavailable!");
 	}
 }
